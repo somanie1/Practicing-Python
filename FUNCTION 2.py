563398e523d4470b2df6c6a59c2d77bbab3492bd
Stephanie Onyiwa <79977982+somanie1@users.noreply.github.com>
2021-06-21 15:10:36 -0700

    Add files via upload

--- FUNCTION 2.py
+++ FUNCTION 2.py
@@ -1,10 +1,11 @@
 
 #function to sum the numbers in a list
 
-def sum_list(a,b,c,d,e):
+def sum_list(numbers):
 
-    return (a+b+c+d+e)
+    return (sum(numbers)) #function will return the sum of any numbers in the bracket
 
-print(sum_list(1,2,3,4,5,))
+
+print(sum_list([1,2,3,4,5,6])) #function will print the sum of numbers in the list given.
 
 
